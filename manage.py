5fb982c7d68b1260b9f88d56eae5ebe14bb2876b
vuluc88 <vuluc88@gmail.com>
2018-12-19 20:28:29 +0700

    run dev in port 5000

--- manage.py
+++ manage.py
@@ -5,9 +5,10 @@ from dialog_flow import dialog
 ##
 ## setup the Api resource routing here
 ##
+api.add_resource(dialog.Webhook, '/webhook')
 api.add_resource(dialog.IntentsList, '/intents')
 api.add_resource(dialog.Intent, '/intents/<string:intent_id>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='8000')
+    app.run(debug=True, host='0.0.0.0', port='5000')
